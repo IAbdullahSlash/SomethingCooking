7d467d0a5734eb96630591f3241e0a0ce1941559
IAbdullahSlash <144207221+IAbdullahSlash@users.noreply.github.com>
2025-10-07 02:03:19 +0530

    fixed image issue

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -44,7 +44,6 @@ export function CTA() {
           </div>
 
           {/* Background decorations */}
-          <div className="absolute inset-0 bg-[url('/grid.svg')] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]" />
           <div className="absolute top-0 left-0 w-72 h-72 bg-white/10 rounded-full blur-3xl" />
           <div className="absolute bottom-0 right-0 w-72 h-72 bg-white/10 rounded-full blur-3xl" />
         </div>
